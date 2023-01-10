7d5257f28cbe87534f3df0e6bc1a0158f2ca1010
Yuliia Panchuk <55352643+YuliiaPanchuk@users.noreply.github.com>
2023-01-10 08:49:02 +0100

    Move files to components folder

---
+++ client/src/components/LinkRow.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { QrCode } from './QrCode';
+
+interface LinkRowProps {
+  list: any[];
+  onDelete: () => void;
+}
+
+export function LinkRow({ list, onDelete }: LinkRowProps) {
+  return (
+    <div className="list">
+      <button onClick={onDelete} className="clear_button">
+        Clear
+      </button>
+      {list.map((item, index) => (
+        <table className="list_urls" key={index}>
+          <thead>
+            <tr>
+              <th>Long url</th>
+              <th>Shorten url</th>
+              <th>Clicked</th>
+              <th>Created at</th>
+              <th>QrCode</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{item.long_url}</td>
+              <td>
+                <a href={item.short_url} target="_blank" rel="noreferrer">
+                  {item.short_url}
+                </a>
+              </td>
+              <td>{item.count}</td>
+              <td>{new Date(item.created_at).toLocaleString()}</td>
+              <td>
+                <QrCode text={item.short_url} />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      ))}
+    </div>
+  );
+}
