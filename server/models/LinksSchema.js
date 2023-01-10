18d57991bae7da9ad0705e3af9dac222f09351ac
Yuliia Panchuk <55352643+YuliiaPanchuk@users.noreply.github.com>
2023-01-10 08:48:22 +0100

    Create Links Schema file

---
+++ server/models/LinksSchema.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose")
+
+const LinksSchema = new mongoose.Schema({
+  _id: {
+    type: String,
+    required: true,
+  },
+  long_url: {
+    type: String,
+    required: true,
+  },
+  short_url: {
+    type: String
+  },
+  created_at: {
+    type: Date,
+    required: true
+  },
+  count: {
+    type: Number,
+    required: true
+  }
+})
+
+const Links = mongoose.model('Link', LinksSchema)
+
+module.exports = { Links }
